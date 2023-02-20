261ab4eef8f9ba5b3feca00e9a677c4e5cba3a1c
Nadine Machmeier <info@nadinemachmeier.com>
2023-02-20 21:41:15 +0000

    itertation 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import foods from './foods.json';
 import { useState } from 'react';
+import FoodBox from './components/FoodBox';
 
 function App() {
   const [list, setList] = useState(foods);
@@ -16,6 +17,13 @@ function App() {
       </div>
       )
     })}
+
+    <FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+}} />
       
     </div>
   );
